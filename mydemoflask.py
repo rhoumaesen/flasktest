7296a93504dcfd5b142b4b63710dc036089ef9cc
rhoouma <71633763+rhoumaesen@users.noreply.github.com>
2022-12-28 05:43:39 +0000

    i changed a line

--- mydemoflask.py
+++ mydemoflask.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 print("hello test")
 @app.route("/")
 def hello():
-    return "Hello! This is a basic Flask app. "
+    return "hello world. "
 
 if __name__ == "__main__":
     app.run("0.0.0.0")
